507e44a921c7b188784b46a13099eab8a7d8ce08
majito <mariajoseportill2@gmail.com>
2025-03-01 07:37:47 -0600

    commit #1 clase busqueda

---
+++ SerieII/PrimerParcial/PrimerParcial/BusquedaBinaria.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimerParcial
+{
+    internal class BusquedaBinaria
+    {
+        public static int BusquedaBinaria(int[] arreglo, int objetivo)
+        {
+            int izquierda = 0, derecha = arreglo.Length - 1;
+            while (izquierda <= derecha)
+            {
+                int medio = izquierda + (derecha - izquierda) / 2;
+                if (arreglo[medio] == objetivo)
+                    return medio;
+                if (arreglo[medio] < objetivo)
+                    izquierda = medio + 1;
+                else
+                    derecha = medio - 1;
+            }
+            return -1;
+        }
+    }
+}
---
+++ SerieII/PrimerParcial/PrimerParcial/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PrimerParcial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Busqueda Binaria");
+            Console.WriteLine("Ingrese el tamaño de su arreglo: ");
+            int tamaño = int.Parse(Console.ReadLine());
+            int[] arreglo = new int[tamaño];
+            Console.WriteLine("Ingrese los elementos de su arreglo: ");
+            for (int i = 0; i < tamaño; i++)
+            {
+                arreglo[i] = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
