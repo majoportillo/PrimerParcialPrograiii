b270e29f6dbb395445e362f24a7e9c18583a876e
majito <mariajoseportill2@gmail.com>
2025-03-01 08:21:57 -0600

    Commit #6 comentarios

--- SerieIII/SerieIIIParcial/SerieIIIParcial/Program.cs
+++ SerieIII/SerieIIIParcial/SerieIIIParcial/Program.cs
@@ -7,27 +7,27 @@ namespace SerieIIIParcial
     {
         static void Main(string[] args)
         {
-            Queue<string> cola = new Queue<string>();
+            Queue<string> cola = new Queue<string>(); //crea una cola
             cola.Enqueue("luisa");
             cola.Enqueue("Ana");
-            cola.Enqueue("Fernando");
+            cola.Enqueue("Fernando"); //agrega elementos a la cola
             cola.Enqueue("Pedro");
             cola.Enqueue("Maria");
 
             Console.WriteLine("Personas en la cola: ");
             foreach (string persona in cola)
             {
-                Console.WriteLine(persona);
+                Console.WriteLine(persona); //muestra los elementos de la cola
             }
 
-            Console.WriteLine("Personas que salen de la cola: ");
+            Console.WriteLine("Personas que salen de la cola: "); //vacia la cola
             while(cola.Count >0)
             {
                 string persona = cola.Dequeue();
                 Console.WriteLine(persona);
             }
 
-            Console.WriteLine("Personas actuales en la cola:");
+            Console.WriteLine("Personas actuales en la cola:"); //muestra los elementos de la cola osea cola ya vacia!!
             foreach (string persona in cola)
             {
                 Console.WriteLine(persona);
