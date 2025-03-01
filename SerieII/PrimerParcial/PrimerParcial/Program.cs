4fa1da1069cc2d175c0cb318c4e08f84b0e45e23
majito <mariajoseportill2@gmail.com>
2025-03-01 07:49:03 -0600

    commit #3 comentarios

--- SerieII/PrimerParcial/PrimerParcial/Program.cs
+++ SerieII/PrimerParcial/PrimerParcial/Program.cs
@@ -10,14 +10,14 @@ namespace PrimerParcial
             Console.WriteLine("Ingrese el tamaño de su arreglo: ");
             int tamaño = int.Parse(Console.ReadLine());
             int[] arreglo = new int[tamaño];
-            Console.WriteLine("Ingrese los elementos de su arreglo: ");
+            Console.WriteLine("Ingrese los elementos de su arreglo (ingrese numero y de enter) : "); //ingresa los elementos al dar enter, no comas ni espacios
             for (int i = 0; i < tamaño; i++)
             {
-                arreglo[i] = int.Parse(Console.ReadLine());
+                arreglo[i] = int.Parse(Console.ReadLine());  
             }
             Console.WriteLine("Ingrese el número que desea buscar: ");
             int objetivo = int.Parse(Console.ReadLine());
-            int resultado = BusquedaBinaria.Buscar(arreglo, objetivo);
+            int resultado = BusquedaBinaria.Buscar(arreglo, objetivo); //busca el numero en el arreglo atraves de la clase BusquedaBinaria
             if (resultado == -1)
                 Console.WriteLine("El número no se encuentra en el arreglo.");
             else
